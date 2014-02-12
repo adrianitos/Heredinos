25ba84c33a12464bc62432924ab7171ab4ec922e
Burnaron <chemenitolis@gmail.com>
2014-02-12 04:50:12 -0800

    creada classe UserVO amb el constructor, amb els setters & getters i un mètode toString.

---
+++ Heredin/src/project/vo/UserVO.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package project.vo;
+
+
+public class UserVO {
+	
+	private String nomUser;
+	private String password;
+	
+	
+	/**
+	 * @param nomUser
+	 * @param password
+	 */
+	public UserVO(String nomUser, String password) {
+		super();
+		this.nomUser = nomUser;
+		this.password = password;
+	}
+	/**
+	 * @return the nomUser
+	 */
+	public String getNomUser() {
+		return nomUser;
+	}
+	/**
+	 * @param nomUser the nomUser to set
+	 */
+	public void setNomUser(String nomUser) {
+		this.nomUser = nomUser;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "UserVO [nomUser=" + nomUser + ", password=" + password + "]";
+	}
+	
+	
+
+}
