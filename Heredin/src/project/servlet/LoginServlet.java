63334981318091121107a30829d802e3458e733a
adrianitos <adriafoncillasmartinez@hotmail.com>
2014-02-13 01:21:58 -0800

    Add LoginServlet

---
+++ Heredin/src/project/servlet/LoginServlet.java
@@ -0,0 +1,25 @@
+package project.servlet;
+
+import java.io.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import project.dao.UserDao;
+
+public class LoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String nomUser=request.getParameter("nomUser");
+		String password=request.getParameter("password");
+		  
+		UserDao miUser = new UserDao();
+		    
+		miUser.CrearUsuari(nomUser,password);
+	   
+		  
+	}
+}
